cdf09ea490e46792b2aee0164112d899fdffb002
fiona-chau <21chauf89@stu.smuhsd.org>
2018-09-12 10:16:15 -0700

    calculate library

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,11 @@
+//this is client code for calculate library
+// @author: Fiona Chau
+// @version: 9/6
+public class DoMath {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
---
+++ Calculate-Library/src/Calculate.java
@@ -0,0 +1,10 @@
+//this class contains the methods to do various math related tasks
+// @ author: Fiona Chau
+// @ version: 9/6
+public class Calculate {
+	
+	public int square(int number)
+	{
+	return number*number;	
+	}
+}
